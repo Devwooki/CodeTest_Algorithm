97fa244c3e926041e48e13f010f4724f75515a2f
wook <dldnr789@gmail.com>
2022-09-27 01:17:00 +0900

    백준 11653번 문제 풀이 추가

--- BOJ_11653/src/Main.java
+++ BOJ_11653/src/Main.java
@@ -1,5 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    /*public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int number = Integer.parseInt(br.readLine());
+        int div_value = 2;
+        while(true){
+            if(number % div_value == 0){
+                number /=div_value;
+                System.out.println(div_value);
+            }else if(div_value > number){
+                break;
+            }else{
+                div_value++;
+                continue;
+            }
+
+        }
+    }*/
+
+
+    //시간 복잡도 개선 및 코드 변경
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int number = Integer.parseInt(br.readLine());
+        int div_value = 2;
+        while(number>1){
+            if(number % div_value == 0){
+                number /=div_value;
+                System.out.println(div_value);
+            }else{
+                div_value++;
+            }
+
+        }
     }
 }
--- BOJ_2581/src/BOJ_2581.java
+++ BOJ_2581/src/BOJ_2581.java
@@ -1,10 +1,8 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.StringTokenizer;
-import java.util.Scanner;
 
-public class Main {
+public class BOJ_2581 {
     public static void main(String[] args) throws IOException{
         BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
 
