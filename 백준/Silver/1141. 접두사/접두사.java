84354c8db3b964432291286d11e2f78d06b2fa31
WOOKI <dldnr789@gmail.com>
2024-01-30 19:04:14 +0900

    [Silver I] Title: 접두사, Time: 132 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Silver/1141. 접두사/접두사.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        List<String> list = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            list.add(br.readLine());
+        }
+        Collections.sort(list, (str1, str2) -> str1.length() - str2.length());
+        //System.out.println(list.toString());
+
+        int noPrefixCnt = N;
+
+        for (int i = 0; i < N; i++) {
+            String baseWord = list.get(i);
+            for (int j = i + 1; j < N; ++j) {
+                if (list.get(j).startsWith(baseWord)) {
+                    noPrefixCnt -= 1;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(noPrefixCnt);
+
+    }
+}
