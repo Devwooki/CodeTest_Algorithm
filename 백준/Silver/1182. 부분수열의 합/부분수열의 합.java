39a35d2ca5231f8212506d4cdb4055a52b7a8504
WOOKI <dldnr789@gmail.com>
2025-10-12 18:56:37 +0900

    [Silver II] Title: 부분수열의 합, Time: 112 ms, Memory: 14568 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,51 +1,48 @@
 import java.io.*;
 import java.util.*;
-import java.util.stream.IntStream;
 
 public class Main {
-    private static int N, S, answer = 0;
-    private static int[] inputs;
-
-    public static void main(String[] args) throws IOException {
+    static int N, S, answer = 0;
+    static int[] map;
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
+        //초기화
         N = Integer.parseInt(st.nextToken());
         S = Integer.parseInt(st.nextToken());
-        inputs = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        map = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
+        //System.out.println("방법 1 : " + getSubSeq());
+    
 
-        //getSubSeq();
-        //System.out.println(answer);
         
-        //수열의 길이가 0인경우가 존재할 수 있기 때문에 0일경우 1을 뺀다
         getSubSeq2(0,0);
         System.out.println(S == 0 ? --answer : answer);
     }
 
-    private static void getSubSeq(){
+    private static int getSubSeq(){
+        int cnt = 0;
 
-        for (int i = 1; i < ( 1 << N); i++) {
+        for(int i = 1 ; i < ( 1 << N) ; ++i){
             int sum = 0;
-            for (int j = 0 ; j < N; j++) {
-                if((i & (1 << j)) != 0){
-                    //System.out.print(inputs[j] + " ");
-                    sum += inputs[j];
-                }
+            for(int j = 0 ; j < N ; ++j){
+                if((i & ( 1 << j )) != 0) sum += map[j];
             }
-            //System.out.println(" : 최종합 = " + sum);
-            if(sum == S) answer +=1;
+
+            if(sum == S) cnt ++;
         }
+
+        return cnt;
     }
 
     private static void getSubSeq2(int sum, int cnt){
         if(cnt == N){
-            if(sum == S)answer++;
+            if(sum == S) answer++;
             return;
         }
 
         getSubSeq2(sum, cnt+1);
-        getSubSeq2(sum + inputs[cnt], cnt+1);
+        getSubSeq2(sum + map[cnt], cnt+1);
     }
-
 }
